870695535bf02e1b4fb00c4e5912336990932e59
Pundima-Lakshan <pundimal@gmail.com>
2025-06-13 16:21:01 +0530

    addd transactions api

---
+++ transaction-api/src/main/java/com/veritas/transaction/api/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Configuration classes. WebClient configuration for making HTTP requests to other services in a load-balanced manner.
+ */
+package com.veritas.transaction.api.config;
---
+++ transaction-api/src/main/java/com/veritas/transaction/api/controller/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Controller classes that handle HTTP requests, provide endpoints for processing transactions.
+ * Implementing circuit breaker and resilience patterns for handling exceptions and timeouts.
+ */
+package com.veritas.transaction.api.controller;
